be103b9d4c149631b1ddb06278a383b4bbe04d82
Nblazer <111671935+Nblazer@users.noreply.github.com>
2026-06-09 10:28:54 -0400

    Refactor socket.io connection handling and player data

--- server.js
+++ server.js
@@ -2,26 +2,65 @@ const http = require('http');
 const { Server } = require('socket.io');
 
 const server = http.createServer();
-const io = new Server(server, { cors: { origin: "*" } });
 
-let players = {};
-
-io.on('connection', (socket) => {
-  socket.on('initialize', () => {
-    players[socket.id] = { x: 0, y: 0, z: 0 };
-    socket.emit('playerData', { id: socket.id, players });
-    socket.broadcast.emit('playerJoined', { id: socket.id });
-  });
-
-  socket.on('positionUpdate', (data) => {
-    players[socket.id] = data;
-    socket.broadcast.emit('playerMoved', { id: socket.id, position: data });
-  });
-
-  socket.on('disconnect', () => {
-    delete players[socket.id];
-    io.emit('playerLeft', { id: socket.id });
-  });
+const io = new Server(server, {
+    cors: {
+        origin: '*'
+    }
 });
 
-server.listen(process.env.PORT || 3000, () => console.log('Server running!'));
+const players = {};
+
+io.on('connection', (socket) => {
+
+    console.log('Connected:', socket.id);
+
+    socket.on('initialize', () => {
+
+        players[socket.id] = {
+            x: 0,
+            y: 0,
+            z: 0
+        };
+
+        socket.emit('playerData', {
+            id: socket.id,
+            players: players
+        });
+
+        socket.broadcast.emit('playerJoined', {
+            id: socket.id
+        });
+
+        console.log('Initialized:', socket.id);
+    });
+
+    socket.on('positionUpdate', (data) => {
+
+        players[socket.id] = {
+            x: data.x,
+            y: data.y,
+            z: data.z
+        };
+
+        socket.broadcast.emit('playerMoved', {
+            id: socket.id,
+            position: players[socket.id]
+        });
+    });
+
+    socket.on('disconnect', () => {
+
+        console.log('Disconnected:', socket.id);
+
+        delete players[socket.id];
+
+        io.emit('playerLeft', {
+            id: socket.id
+        });
+    });
+});
+
+server.listen(process.env.PORT || 3000, () => {
+    console.log('Server running!');
+});
